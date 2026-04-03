087632815a1fcc73707d79b7bb7cf37547125e73
Sayed Imran <sayedimran00786@gmail.com>
2026-04-03 15:27:48 +0530

    Refactor kind installation script for OS and ARCH

--- 0-install-kind.sh
+++ 0-install-kind.sh
@@ -1,6 +1,7 @@
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-arm64
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+[ "$ARCH" = "x86_64" ] && ARCH="amd64"
+[ "$ARCH" = "aarch64" ] && ARCH="arm64"
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.31.0/kind-${OS}-${ARCH}"
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
